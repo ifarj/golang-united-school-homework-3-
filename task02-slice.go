6f4586cc021b279ffe63d33757be262fd36679d8
Berik Akhmed <a.berik@bsl.dev>
2022-05-12 01:06:07 +0600

    finish task2

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,12 @@
 package homework
 
-func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+import "sort"
+
+func reverse(input []int64) []int64 {
+	result := make([]int64, len(input))
+	sort.Slice(input, func(i, j int) bool { return input[i] > input[j] })
+	for i := range input {
+		result[i] = input[i]
+	}
+	return result
 }
