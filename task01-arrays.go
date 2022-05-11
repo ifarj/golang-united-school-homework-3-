51849ae66158ddebfe3d4cb922f2a3f42a69ea1d
Berik Akhmed <a.berik@bsl.dev>
2022-05-12 00:49:25 +0600

    finish task1

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,8 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	for i := range input {
+		result += input[i]
+	}
+	return result / float32(len(input))
 }
