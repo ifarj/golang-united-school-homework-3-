59684e8c0a62828e34a8666f7e7c01502bf8b87b
Berik Akhmed <a.berik@bsl.dev>
2022-05-12 01:15:35 +0600

    finish task3

--- task02-slice.go
+++ task02-slice.go
@@ -3,10 +3,6 @@ package homework
 import "sort"
 
 func reverse(input []int64) []int64 {
-	result := make([]int64, len(input))
 	sort.Slice(input, func(i, j int) bool { return input[i] > input[j] })
-	for i := range input {
-		result[i] = input[i]
-	}
-	return result
+	return input
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,15 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	arrKeys := make([]int, 0)
+	for k := range input {
+		arrKeys = append(arrKeys, k)
+	}
+	sort.Slice(arrKeys,  func(i, j int) bool { return input[i] < input[j] })
+	for i := range arrKeys {
+		result = append(result, input[arrKeys[i]])
+	}
 	return
 }
